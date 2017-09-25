f349d8dfcaf056ba82b1d40da14eb42d6d1ba665
ANNIELIN <a12lin@gmail.com>
2017-09-24 18:15:53 -0700

    Upload Homework 2

---
+++ annie-a12lin/Homework-2/6-temp-converter/js/main.js
@@ -0,0 +1,76 @@
+/*
+1. Create a function that accepts a temperature value in Fahrenheit and converts it to Celsius.
+   Note: Equation to convert F to C:
+   C = (F - 32) / 1.8
+   Test your function.
+*/
+
+
+/* HOMEWORK #1 BELOW
+let fahrenheit = window.prompt("What temperature in Fahrenheit do you want to convert to Celsius?");
+
+function fToC(temp) {
+    console.log((temp - 32) / 1.8);
+}
+
+fToC(fahrenheit);
+/*
+
+
+
+
+
+/*
+2. Create a second function that accepts a temperature value in Celsius and converts it to Fahrenheit.
+   Note: Equation to convert C to F:
+   F = 1.8 * C + 32
+   Test your function.
+*/
+
+
+/* HOMEWORK #2 BELOW
+let celsius = window.prompt("What temperature in Celsius do you want to convert to Fahrenheit?");
+
+function cToF(temp) {
+    console.log((1.8 * temp) + 32);
+}
+
+cToF(celsius);
+*/
+
+
+
+
+
+/*
+BONUS 1: Update your functions to use the value entered in the text box on the web page, and to perform calculations only when the appropriate button is clicked:
+	1.  Set the value of the temp variable to the value of the element with the id value 'temperature'.   
+	    (Hint: read about the document.querySelector() method.)
+	2.  Set the appropriate function to run when each button is clicked.
+        (Hint: read about the addEventListener() method.)
+
+BONUS 2: Display the result of each calculation in the browser window rather than in the console:
+	1.  At the end of each function, set the text of the element with the id value 'result' to the same text string you were previously logging to the console.
+BONUS 3: Clear the contents of the text box after you've displayed your results, so a user can easily enter another temperature to convert. (Hint: Read about the 'value' property in JavaScript. What string value could you assign as the value of an input box that would give it no content to display?)
+*/
+
+
+let temp = document.querySelector("#temperature").value;
+
+document.getElementById("fahrenheit_to_celsius").addEventListener("click", fToC);
+
+function fToC() {
+    let answer = 1.8 * temp + 32;
+    document.getElementById("result").innerHTML = answer;
+    document.getElementById("temperature").value = " ";
+}
+
+document.getElementById("celsius_to_fahrenheit").addEventListener("click", cToF);
+
+function cToF() {
+    let answer = (temp - 32) / 1.8;
+    document.getElementById("result").innerHTML = answer;
+    document.getElementById("temperature").value = " ";
+}
+
+
